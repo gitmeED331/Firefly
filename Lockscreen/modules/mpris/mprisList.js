3ab2a7c3bdf8fe17f1cc9f44fc18b7b1f00ca584
Topsy Krypts <142960718+gitmeED331@users.noreply.github.com>
2024-04-27 22:00:16 -0500

    Add files via upload

---
+++ Lockscreen/modules/mpris/mprisList.js
@@ -0,0 +1,171 @@
+import Widget, {Box} from "resource:///com/github/Aylur/ags/widget.js";
+import icons from "../icons/index.js";
+import Mpris from "resource:///com/github/Aylur/ags/service/mpris.js";
+import {lookUpIcon, execAsync} from "resource:///com/github/Aylur/ags/utils.js";
+
+/**
+ * @param {string} coverPath
+ */
+async function blurCoverArtCss(coverPath) {
+
+  /** @param {string} bg
+  *   @param {string} color
+  */
+  const genCss = (bg, color) =>
+    `background-image: radial-gradient(
+      circle at right,
+      rgba(0, 0, 0, 0),
+      ${color} 11.5rem), url('${bg}');
+    background-position: right top, right top;
+    background-size: contain;
+    transition: all 0.7s ease;
+    background-repeat: no-repeat;`;
+
+  if(coverPath) {
+    const color = await execAsync(`bash -c "convert ${coverPath} -alpha off -crop 5%x100%0+0+0 -colors 1 -unique-colors txt: | head -n2 | tail -n1 | cut -f4 -d' '"`);
+    return genCss(coverPath, color);
+  }
+  return "background-color: #0e0e1e";
+}
+
+/**
+ * @param {import('types/service/mpris').MprisPlayer} player
+ * @param {import('types/widgets/icon').Props} props
+ */
+export const PlayerIcon = (player, { ...props } = {}) => {
+  const icon = lookUpIcon(player.entry)
+    ? player.entry
+    : icons.mpris.fallback;
+  return Widget.Icon({
+    ...props,
+    class_name: "music-player-icon",
+    icon: icon,
+  });
+};
+
+/**
+ * @param {import('types/service/mpris').MprisPlayer} player
+ */
+export const MprisPlayer = player => Widget.Box({
+  class_name: "music-container",
+  vertical: true,
+  children: [
+    Widget.Box({
+      tooltip_text: player.identity || "",
+      children: [
+        PlayerIcon(player),
+        Widget.Label({
+          class_name: "music-player-name",
+          label: player.identity
+        })
+      ]
+    }),
+    Widget.Box({
+      vertical: true,
+      children: [
+        Widget.Box({
+          vertical: true,
+          children: [
+            Widget.Label({
+              max_width_chars: 35,
+              truncate: "end",
+              class_name: "music-title",
+              xalign: 0,
+              label: player.bind("track_title")
+            }),
+            Widget.Label({
+              max_width_chars: 35,
+              truncate: "end",
+              class_name: "music-artist",
+              xalign: 0,
+              label: player.bind("track_artists").transform(art => art.join(", "))
+            }),
+          ]
+        }),
+        Widget.Box({
+          class_name: "music-control-box",
+          children: [
+            Widget.Box({
+              class_name: "spacing-10",
+              vpack: "center",
+              children: [
+                Widget.Button({
+                  class_name: "music-button",
+                  on_clicked: () => player.previous(),
+                  child: Widget.Icon(icons.mpris.prev),
+                }),
+                Widget.Button({
+                  on_clicked: () => player.playPause(),
+                  child: Widget.CircularProgress({
+                    class_name: "music-progress",
+                    start_at: 0.75,
+                    child: Widget.Icon({
+                      class_name: "music-button",
+                    })
+                      .hook(Mpris, (icon) => {
+                        let icn = icons.mpris.stopped;
+                        if (player.play_back_status === "Playing")
+                          icn = icons.mpris.playing;
+                        else if (player.play_back_status === "Paused")
+                          icn = icons.mpris.paused;
+                        icon.icon = icn;
+                      }),
+                  })
+                    .hook(player, (prog) => {
+                      if (!player) return;
+                      prog.value = player.position / player.length;
+                    })
+                    .poll(1000, (prog) => {
+                      if (!player) return;
+                      prog.value = player.position / player.length;
+                    })
+                }),
+                Widget.Button({
+                  class_name: "music-button",
+                  on_clicked: () => player.next(),
+                  child: Widget.Icon(icons.mpris.next)
+                }),
+              ]
+            }),
+          ]
+        })
+      ]
+    })
+  ]
+}).hook(player, async (self) => {
+  self.css = await blurCoverArtCss(player.cover_path);
+}, "notify::cover-path");
+
+
+const PlayerList = () => Box({
+  class_name: "spacing-5",
+  hexpand: true,
+  vertical: true,
+  attribute: {
+    "player": new Map(),
+    /**
+     * @param {import('types/widgets/box').default} box
+     * @param {string} id
+    */
+    "onAdded": (box, id) => {
+      const player = Mpris.getPlayer(id);
+      if (!id || !player || box.attribute.player.has(id)) return;
+      const playerWidget = MprisPlayer(player);
+      box.attribute.player.set(id, playerWidget);
+      box.pack_start(playerWidget, false, false, 0);
+    },
+    /**
+     * @param {import('types/widgets/box').default} box
+     * @param {string} id
+    */
+    "onRemoved": (box, id) => {
+      if (!id || !box.attribute.player.has(id)) return;
+      box.attribute.player.get(id).destroy();
+      box.attribute.player.delete(id);
+    }
+  }
+})
+  .hook(Mpris, (box, id) => box.attribute.onAdded(box, id), "player-added")
+  .hook(Mpris, (box, id) => box.attribute.onRemoved(box, id), "player-closed");
+
+export default PlayerList;
---
+++ Lockscreen/modules/powermenu/index.js
@@ -0,0 +1,125 @@
+import icons from "../icons/index.js";
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import {execAsync} from "resource:///com/github/Aylur/ags/utils.js";
+import App from "resource:///com/github/Aylur/ags/app.js";
+import RoundedCorner, { RoundedAngleEnd } from "../roundedCorner/index.js";
+
+/**
+ * @param {string} name
+ * @param {string} icon
+ * @param {function(import('types/widgets/button').default): void} command
+ */
+const SessionButton = (name, icon, command, props = {}) => {
+  const buttonDescription = Widget.Revealer({
+    vpack: "end",
+    transition_duration: 200,
+    transition: "slide_down",
+    reveal_child: false,
+    child: Widget.Label({
+      class_name: "session-button-desc",
+      label: name,
+    }),
+  });
+  return Widget.Button({
+    on_clicked: command,
+    class_name: "session-button",
+    cursor: "pointer",
+    on_hover: self => {
+      buttonDescription.reveal_child = true;
+      self.toggleClassName("session-button-focused", true);
+    },
+    on_hover_lost: self => {
+      buttonDescription.reveal_child = false;
+      self.toggleClassName("session-button-focused", false);
+    },
+    child: Widget.Overlay({
+      class_name: "session-button-box",
+      child: Widget.Icon({
+        class_name: "session-button-icon",
+        icon: icon,
+      }),
+      overlays: [
+        buttonDescription,
+      ]
+    }),
+    ...props,
+  });
+  // .on("focus-in-event", (self) => {
+  //   buttonDescription.reveal_child = true;
+  //   self.toggleClassName("session-button-focused", true);
+  // })
+  // .on("focus-out-event", (self) => {
+  //   buttonDescription.reveal_child = false;
+  //   self.toggleClassName("session-button-focused", false);
+  // });
+};
+
+const SessionBox = () => {
+  const lockButton = SessionButton("Lock", icons.powermenu.lock, () => {
+    App.closeWindow("session");
+    execAsync(`ags -b lockscreen -c ${App.configDir}/lockscreen.js`);
+  });
+  const logoutButton = SessionButton("Logout", icons.powermenu.logout, () => {
+    App.closeWindow("session");
+    execAsync(["bash", "-c", "loginctl terminate-user $USER"]);
+  });
+  const sleepButton = SessionButton("Sleep", icons.powermenu.sleep, () => {
+    App.closeWindow("session");
+    execAsync("systemctl suspend");
+  });
+  const shutdownButton = SessionButton("Shutdown", icons.powermenu.shutdown, () => {
+    App.closeWindow("session");
+    execAsync("systemctl poweroff");
+  });
+  const rebootButton = SessionButton("Reboot", icons.powermenu.reboot, () => {
+    App.closeWindow("session");
+    execAsync("systemctl reboot");
+  });
+  return Widget.Box({
+    children: [
+      lockButton,
+      logoutButton,
+      sleepButton,
+      shutdownButton,
+      rebootButton,
+    ]
+  });
+};
+
+const SessionRevealer = () => Widget.Revealer({
+  transition: "slide_down",
+  reveal_child: false,
+  child: Widget.Box({
+    vertical: true,
+    children: [
+      Widget.Box({
+        children: [
+          SessionBox(),
+          RoundedAngleEnd("topright", {class_name: "angle"})
+        ]
+      }),
+      RoundedCorner("topleft", {class_name: "corner"})
+    ]
+  })
+})
+  .hook(App, (rev, windowName, visible) => {
+    if(windowName === "session")
+      rev.reveal_child = visible;
+  }, "window-toggled");
+
+
+export default () => Widget.Window({
+  name: "session",
+  class_name: "session",
+  layer: "overlay",
+  visible: false,
+  anchor: ["top", "left"],
+  child: Widget.Box({
+    vertical: true,
+    css: "min-height: 2px;",
+    children: [
+      SessionRevealer(),
+    ]
+  }),
+});
+
